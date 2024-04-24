00afdab5586837a804ef83ae93b8be92640a0b44
Arnaud LECHAT <94056904+4rn4ud@users.noreply.github.com>
2024-04-24 12:30:32 +0200

    Update DashboardController.php

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Appartenir;
+use App\Entity\Equipe;
+use App\Entity\Joueur;
+use App\Entity\Partie;
+
+use App\Controller\IndexController;
+
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +21,12 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+        
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = ($routeBuilder->setController(JoueurCrudController::class)->generateUrl());
+        
+        return $this->redirect($url);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -40,7 +53,12 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'app_index');
++       yield MenuItem::linkToCrud('Joueurs', 'fas fa-list', Joueur::class);
++       yield MenuItem::linkToCrud('Appartenirs', 'fas fa-list', Appartenir::class);
+        yield MenuItem::linkToCrud('Equipes', 'fas fa-list', Equipe::class);
+        yield MenuItem::linkToCrud('Parties', 'fas fa-list', Partie::class);
     }
 }
