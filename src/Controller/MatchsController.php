d9f97d491fe76fd4ca1f0ae2a212868d3887726e
Arnaud LECHAT <94056904+4rn4ud@users.noreply.github.com>
2024-04-25 17:08:04 +0200

    rename MatchsController

--- src/Controller/MatchsController.php
+++ src/Controller/MatchsController.php
@@ -11,14 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/matchs')]
-class MatchController extends AbstractController
-{
-    #[Route('/', name: 'app_matchs', methods: ['GET'])]
-    public function index(PartieRepository $partieRepository): Response
-    {
-        return $this->render('partie/index.html.twig', [
-            'parties' => $partieRepository->findAll(),
-        ]);
-    }
-}
+// #[Route('/matchs')]
+// class MatchController extends AbstractController
+// {
+//     #[Route('/', name: 'app_matchs', methods: ['GET'])]
+//     public function index(PartieRepository $partieRepository): Response
+//     {
+//         return $this->render('matchs.html.twig', [
+//             'parties' => $partieRepository->findAll(),
+//         ]);
+//     }
+// }
