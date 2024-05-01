1103a17f40368eaf7244ff11a3421086b1f0807f
4rn4ud <arnaud.lechat1@gmail.com>
2024-05-01 13:29:08 +0200

    Page matchs

--- src/Controller/MatchsController.php
+++ src/Controller/MatchsController.php
@@ -2,23 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\Partie;
-use App\Form\PartieType;
-use App\Repository\PartieRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-// #[Route('/matchs')]
-// class MatchController extends AbstractController
-// {
-//     #[Route('/', name: 'app_matchs', methods: ['GET'])]
-//     public function index(PartieRepository $partieRepository): Response
-//     {
-//         return $this->render('matchs.html.twig', [
-//             'parties' => $partieRepository->findAll(),
-//         ]);
-//     }
-// }
+#[Route('/matchs')]
+class MatchsController extends AbstractController
+{
+    #[Route('/', name: 'app_matchs')]
+    public function matchs(): Response
+    {
+        return $this->render('matchs.html.twig');
+    }
+}
